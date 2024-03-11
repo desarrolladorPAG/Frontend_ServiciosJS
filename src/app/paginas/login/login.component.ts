8d8eeabe110241d6157abbaca9a0fc4fa2d6d6d6
desarrolladorPAG <desarrollador@pagcolombian.com>
2024-03-11 08:33:33 -0500

    Vista de componente login

---
+++ src/app/paginas/login/login.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  ngOnInit(): void {
+    const sign_in_btn = document.querySelector("#sign-in-btn");
+    const sign_up_btn = document.querySelector("#sign-up-btn");
+    const container = document.querySelector(".contenedor");
+
+    if (sign_up_btn && sign_in_btn && container) {
+      sign_up_btn.addEventListener("click", () => {
+        container.classList.add("sign-up-mode");
+      });
+
+      sign_in_btn.addEventListener("click", () => {
+        container.classList.remove("sign-up-mode");
+      });
+    }
+
+    
+  }
+
+  
+  
+
+
+  
+}
