45dc34201f6bc02697482202e28014bda6bc986a
desarrolladorPAG <desarrollador@pagcolombian.com>
2024-03-11 09:48:03 -0500

    Formulario reactivo de login

--- src/app/paginas/login/login.component.ts
+++ src/app/paginas/login/login.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import {FormBuilder, FormControl, FormGroup, Validator, Validators} from '@angular/forms'
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  submitted = false;
+
+  constructor(private fb : FormBuilder){}
 
   ngOnInit(): void {
+    
+    // Logica para animacion de login
     const sign_in_btn = document.querySelector("#sign-in-btn");
     const sign_up_btn = document.querySelector("#sign-up-btn");
     const container = document.querySelector(".contenedor");
@@ -21,9 +26,18 @@ export class LoginComponent implements OnInit {
         container.classList.remove("sign-up-mode");
       });
     }
-
-    
   }
+
+  form_login : FormGroup = this.fb.group({
+    correo : this.fb.control('', [Validators.required, Validators.email
+    ]),
+    password: this.fb.control('', [Validators.required])
+  });
+
+  login(){
+    this.submitted = true;
+  }
+
 
   
   
